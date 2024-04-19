701803737e1f48e4641c1c219007d5b3aa29ded3
MaracujaDoMack <162309148+MaracujaDoMack@users.noreply.github.com>
2024-04-18 21:20:58 -0300

    Update Projeto1.c

--- Projeto1/Projeto1.c
+++ Projeto1/Projeto1.c
@@ -1,93 +1,91 @@
-#include <malloc.h>
 #include <pthread.h>
-#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-#define _GNU_SOURCE
-#define MAX_CAPACITY 50
+#define MAX_CAPACITY 10000
 #define TRAVEL_TIME 10
 
 typedef struct {
-    int timeArrived;
-    int personDirection;
+  int timeArrived;
+  int personDirection;
 } Person;
 
 int lastMoment = 0;
 int currentDirection = -1;
-sem_t directionSem;
+pthread_mutex_t lock;
 
 void *threadFunction(void *argument) {
-    Person *person = (Person *)argument;
-    int finishTime = person->timeArrived + TRAVEL_TIME;
 
-    sem_wait(&directionSem);
+  Person *person = (Person *)argument;
+  int arrivalTime = person->timeArrived;
+  int direction = person->personDirection;
 
-    if (currentDirection == -1) {
-        currentDirection = person->personDirection;
-    }
+  pthread_mutex_lock(&lock);
 
-    if (finishTime > lastMoment && (person->personDirection == currentDirection || currentDirection == -1)) {
-        lastMoment = finishTime;
-        currentDirection = -1;
-    }
+  if (lastMoment == 0 && currentDirection == -1) {
+    currentDirection = direction;
+    lastMoment = arrivalTime + TRAVEL_TIME;
+  }
 
-    sem_post(&directionSem);
+  if (arrivalTime < lastMoment && direction == currentDirection) {
+    lastMoment = arrivalTime + TRAVEL_TIME;
+  } else {
+    // alguma função que espera
+    lastMoment += TRAVEL_TIME;
+  }
 
-    free(person);
-    return NULL;
+  pthread_mutex_unlock(&lock);
+
+  free(person);
+  return NULL;
 }
 
 int main() {
-  
-    printf("Programa iniciado!\n\n");
+  printf("Programa iniciado!\n\n");
 
-    int amount = 0;
-    pthread_t threadsVector[MAX_CAPACITY];
-    Person peopleVector[MAX_CAPACITY];
-    FILE *input, *output;
+  int amount = 0;
+  pthread_t threadsVector[MAX_CAPACITY];
+  Person peopleVector[MAX_CAPACITY];
+  FILE *input, *output;
 
-    input = fopen("input.txt", "r");
-    output = fopen("output.txt", "w");
-    if (input == NULL || output == NULL) {
-        printf("Erro! Arquivos não foram abertos corretamente!\n\n");
-        return 1;
-    }
+  input = fopen("input.txt", "r");
+  output = fopen("output.txt", "w");
+  if (input == NULL || output == NULL) {
+    printf("Erro! Arquivos não foram abertos corretamente!\n\n");
+    return 1;
+  }
 
-    sem_init(&directionSem, 0, 1);
+  fscanf(input, "%d", &amount);
 
-    fscanf(input, "%d", &amount);
+  if (amount < 1 || amount > MAX_CAPACITY) {
+    printf("Erro! Quantidade de pessoas inválida!\n\n");
+    return 1;
+  }
 
-    if (amount < 1 || amount > MAX_CAPACITY) {
-        printf("Erro! Quantidade de pessoas inválida!\n\n");
-        return 1;
-    }
+  for (int i = 0; i < amount; i++) {
+    fscanf(input, "%d %d", &peopleVector[i].timeArrived, &peopleVector[i].personDirection);
+  }
+  fclose(input);
 
-    for (int i = 0; i < amount; i++) {
-        fscanf(input, "%d %d", &peopleVector[i].timeArrived, &peopleVector[i].personDirection);
-    }
-    fclose(input);
+  pthread_mutex_init(&lock, NULL);
 
-    currentDirection = peopleVector[0].personDirection;
+  for (int i = 0; i < amount; i++) {
+    Person *newPerson = (Person *)malloc(sizeof(Person));
+    newPerson->timeArrived = peopleVector[i].timeArrived;
+    newPerson->personDirection = peopleVector[i].personDirection;
+    pthread_create(&threadsVector[i], NULL, threadFunction, (void *)newPerson);
+  }
 
-    for (int i = 0; i < amount; i++) {
-        Person *newPerson = (Person *)malloc(sizeof(Person));
-        newPerson->timeArrived = peopleVector[i].timeArrived;
-        newPerson->personDirection = peopleVector[i].personDirection;
-        pthread_create(&threadsVector[i], NULL, threadFunction, (void *)newPerson);
-    }
+  for (int i = 0; i < amount; i++) {
+    pthread_join(threadsVector[i], NULL);
+  }
 
-    for (int i = 0; i < amount; i++) {
-        pthread_join(threadsVector[i], NULL);
-    }
+  fprintf(output, "Tempo gasto: %d segundos\n", lastMoment);
+  fclose(output);
 
-    fprintf(output, "Tempo gasto: %d segundos\n", lastMoment);
-    fclose(output);
+  pthread_mutex_destroy(&lock);
 
-    sem_destroy(&directionSem);
+  printf("Programa finalizado!");
 
-    printf("Programa finalizado!");
-
-    return 0;
+  return 0;
 }
